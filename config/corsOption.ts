8e97f8120312ae0c2083b915d492894a53242554
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-06-26 01:00:45 +0300

    last change

--- config/corsOption.ts
+++ config/corsOption.ts
@@ -1,6 +1,7 @@
 import { CorsOptions } from "cors";
 const allowedOrigins = [
-  "https://www.Dentist_World.onrender.com",
+  "https://www.dentist_world.onrender.com",
+  "https://www.dentists_students_world.onrender.com",
   // "http://localhost:3003",
   // "http://localhost:3001",
   // "http://localhost:3000",
